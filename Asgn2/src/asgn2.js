4b3461703c7553994d32b0dcc5f89874c61df2b2
Alexandra Hatalsky <alexhatalsky@MacBook-Pro.local>
2024-04-25 00:59:56 -0700

    back half of shark working

--- Asgn2/src/asgn2.js
+++ Asgn2/src/asgn2.js
@@ -84,78 +84,38 @@ function connectVariablesToGLSL() {
 // UI element globals
 let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
 let g_selectedSize = 10.0;
-let g_selectedType = 'SQUARE';
+let g_animation = false;
 let g_globalAngle = 0;
+let g_redAngle = 0;
+let g_yellowAngle = 0;
+let g_magentaAngle = 0;
+
 
 
 // Set up actions for the HTML UI elements
 function addActionsForHTML() {
     // Button Events
-    document.getElementById('clearButton').onclick = function() {
-        g_shapesList = [];
-        g_showPicture = 'FALSE';
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        renderAllShapes();
+    document.getElementById('animationOnButton').onclick = function() {
+        g_animation = true;
     };
-    document.getElementById('undoButton').onclick = function() {
-        g_shapesList.pop();
-        renderAllShapes();
+    document.getElementById('animationOffButton').onclick = function() {
+        g_animation = false;
     };
-    document.getElementById('backgroundButton').onclick = function() {
-        g_shapesList = [];
-        g_showPicture = 'FALSE';
-        gl.clearColor(g_selectedColor[0], g_selectedColor[1], g_selectedColor[2], g_selectedColor[3])
-        renderAllShapes();
-    };
-    document.getElementById('randomButton').onclick = function() {
-        var shape = Math.floor(Math.random()*3);
-        if (shape == 0) {
-            g_selectedType = 'SQUARE';
-        } else if (shape == 1) {
-            g_selectedType = 'TRIANGLE';
-        } else {
-            g_selectedType = 'CIRCLE';
-        }
-        
-        let newSize = 5.0 + (Math.random() * 35)
-        g_selectedSize = newSize;
-        let sizeSlide = document.getElementById('sizeSlide')
-        sizeSlide.value = newSize;
-        
-        let redSlide = document.getElementById('redSlide');
-        let newColor = Math.random();
-        g_selectedColor[0] = newColor;
-        redSlide.value = newColor * 100;
-        
-        let greenSlide = document.getElementById('greenSlide');
-        newColor = Math.random();
-        g_selectedColor[1] = newColor;
-        greenSlide.value = newColor * 100;
-        
-        let blueSlide = document.getElementById('blueSlide');
-        newColor = Math.random();
-        g_selectedColor[2] = newColor;
-        blueSlide.value = newColor * 100;
-    };
-    document.getElementById('pictureButton').onclick = function() {
-        g_shapesList = [];
-        g_showPicture = 'TRUE';
-        renderAllShapes();
-    };
-    document.getElementById('squareButton').onclick = function() {g_selectedType = 'SQUARE'; };
-    document.getElementById('triButton').onclick = function() {g_selectedType = 'TRIANGLE'; };
-    document.getElementById('circleButton').onclick = function() {g_selectedType = 'CIRCLE'; };
+
     
     // Slider Events
-    document.getElementById('redSlide').addEventListener('mouseup', function() { g_selectedColor[0] = this.value/100; });
-    document.getElementById('greenSlide').addEventListener('mouseup', function() { g_selectedColor[1] = this.value/100; });
-    document.getElementById('blueSlide').addEventListener('mouseup', function() { g_selectedColor[2] = this.value/100;} );
-    
     document.getElementById('angleSlide').addEventListener('mousemove', function() {
         g_globalAngle = this.value;
-        renderAllShapes();
     });
-    document.getElementById('segSlide').addEventListener('mouseup', function() { g_selectedSegments = this.value; });
+    document.getElementById('redSlide').addEventListener('mousemove', function() {
+        g_redAngle = this.value;
+    });
+    document.getElementById('yellowSlide').addEventListener('mousemove', function() {
+        g_yellowAngle = this.value;
+    });
+    document.getElementById('magentaSlide').addEventListener('mousemove', function() {
+        g_magentaAngle = this.value;
+    });
 }
 
 
@@ -177,7 +137,26 @@ function main() {
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
         // Render
-        renderAllShapes();
+        //renderAllShapes();
+        requestAnimationFrame(tick);
+}
+
+var g_startTime = performance.now()/1000.0;
+var g_seconds = performance.now()/1000.0 - g_startTime;
+
+// Called by browser repeatedly to render the scene
+function tick() {
+    
+    g_seconds = performance.now()/1000.0 - g_startTime;
+    console.log(g_seconds);
+    
+    // Changes the animation angles if needed
+    updateAnimationAngles();
+    
+    // Renders the canvas
+    renderAllShapes();
+    
+    requestAnimationFrame(tick);
 }
 
 
@@ -186,144 +165,130 @@ function main() {
 
 var g_shapesList = [];
 
-function click(ev) {
-    // Extract the event click and return it in WebGL coordinates
-    [x, y] = convertCoordinatesEventToGL(ev);
-    
-    // Create a new point, set its attributes, and push it to the list
-    let point;
-    if (g_selectedType == 'SQUARE'){
-        point = new Square();
-    } else if (g_selectedType == 'TRIANGLE') {
-        point = new Triangle();
-    } else {
-        point = new Circle();
-        point.segments = g_selectedSegments;
+function updateAnimationAngles() {
+    if (g_animation) {
+        g_redAngle = (45 * Math.sin(g_seconds));
+        let redSlide = document.getElementById('redSlide');
+        redSlide.value = g_redAngle;
+        
+        g_yellowAngle = (45 * Math.sin(g_seconds - 1));
+        let yellowSlide = document.getElementById('yellowSlide');
+        yellowSlide.value = g_yellowAngle;
+        
+        g_magentaAngle = (45 * Math.sin(g_seconds - 2));
+        let magentaSlide = document.getElementById('magentaSlide');
+        magentaSlide.value = g_magentaAngle;
     }
-
-    point.position = [x, y];
-    point.color = g_selectedColor.slice();
-    point.size = g_selectedSize;
-    g_shapesList.push(point);
-
-    renderAllShapes();
-}
-
-// Extract the event click and return it in WebGL coordinates
-function convertCoordinatesEventToGL(ev) {
-    var x = ev.clientX; // x coordinate of a mouse pointer
-    var y = ev.clientY; // y coordinate of a mouse pointer
-    var rect = ev.target.getBoundingClientRect();
-
-    x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
-    y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
-
-    return([x, y]);
 }
      
 // Draw every shape that is supposed to be in the canvas
 function renderAllShapes() {
-        // Pass the angle matrix to u_GlobalRotateMatrix
-        var globalRotMat = new Matrix4().rotate(g_globalAngle, 0.0, 1.0, 0.0);
-        gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
-        
-        // Clear <canvas>
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        
-        var body = new Cube();
-        body.color = [1.0, 0.0, 0.0, 1.0];
-        body.matrix.translate(-0.25, -0.5, 0.0);
-        body.matrix.scale(0.5, 1.0, 0.5);
-        body.render();
-        
-        var leftArm = new Cube();
-        leftArm.color = [1.0, 1.0, 0.0, 1.0];
-        leftArm.matrix.translate(0.7, 0.0, 0.0);
-        leftArm.matrix.rotate(45, 0.0, 0.0, 1.0);
-        leftArm.matrix.scale(0.25, 0.7, 0.5);
-        leftArm.render();
-        
-        var box = new Cube();
-        box.color = [1.0, 0.0, 1.0, 1.0];
-        box.matrix.translate(0.0, 0.0, -0.5, 0.0);
-        box.matrix.rotate(-30, 1, 0, 0);
-        box.matrix.scale(0.5, 0.5, 0.5);
-        box.render();
+    // Pass the angle matrix to u_GlobalRotateMatrix
+    var globalRotMat = new Matrix4().rotate(g_globalAngle, 1.0, 0.0, 0.0);
+    gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
+    
+    // Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    
+    var bodyColor = [0.36, 0.54, 0.82, 1.0];
+    
+    // Body of the shark
+    var body = new Cube();
+    body.color = bodyColor;
+    body.matrix.translate(-0.15, -0.35, -0.15);
+    body.matrix.scale(0.3, 0.7, 0.3);
+    body.render();
+    
+    var body2 = new Cube();
+    body2.color = bodyColor;
+    body2.matrix = new Matrix4(body.matrix);
+    body2.matrix.translate(0.5, 0.025, 0.5);
+    body2.matrix.scale(1.0, 0.95, 0.95);
+    body2.matrix.rotate(g_redAngle, 0, 1, 0);
+    body2.matrix.translate(0.0, 0.0, -0.5);
+    body2.render();
+    
+    var body3 = new Cube();
+    body3.color = bodyColor;
+    body3.matrix = new Matrix4(body2.matrix);
+    body3.matrix.translate(0.6, 0.075, 0.07);
+    body3.matrix.scale(1.0, 0.85, 0.85);
+    body3.matrix.rotate(g_redAngle, 0, 1, 0);
+    body2.matrix.translate(0.0, 0.0, -0.5);
+    body3.render();
+    
+    var body4 = new Cube();
+    body4.color = bodyColor;
+    body4.matrix = new Matrix4(body3.matrix);
+    body4.matrix.translate(0.8, 0.15, 0.15);
+    body4.matrix.scale(0.7, 0.7, 0.7);
+    body3.matrix.rotate(g_redAngle, 0, 1, 0);
+    body2.matrix.translate(0.0, 0.0, -0.5);
+    body4.render();
+    
+    var body5 = new Cube();
+    body5.color = bodyColor;
+    body5.matrix = new Matrix4(body4.matrix);
+    body5.matrix.translate(0.6, 0.15, 0.2);
+    body5.matrix.scale(1.0, 0.7, 0.7);
+    body3.matrix.rotate(g_redAngle, 0, 1, 0);
+    body2.matrix.translate(0.0, 0.0, -0.5);
+    body5.render();
+    
+    var fin1 = new Cube();
+    fin1.color = bodyColor;
+    fin1.matrix = new Matrix4(body.matrix);
+    fin1.matrix.translate(0.4, 0.7, 0.45);
+    fin1.matrix.rotate(45, 0, 0, 1);
+    fin1.matrix.scale(0.5, 0.4, 0.1);
+    fin1.render();
+    
+    var fin2 = new Cube();
+    fin2.color = bodyColor;
+    fin2.matrix = new Matrix4(body.matrix);
+    fin2.matrix.translate(0.0, 0.08, 0.2);
+    fin2.matrix.rotate(-45, 0, 0, 1);
+    fin2.matrix.scale(0.35, 0.2, 0.1);
+    fin2.render();
+    
+    var fin3 = new Cube();
+    fin3.color = bodyColor;
+    fin3.matrix = new Matrix4(fin2.matrix);
+    fin3.matrix.translate(0.0, 0.0, 5.0);
+    fin3.render();
+    
+    var backFin1 = new Cube();
+    backFin1.color = bodyColor;
+    backFin1.matrix = new Matrix4(body5.matrix);
+    backFin1.matrix.translate(1.4, 0.55, 0.35);
+    backFin1.matrix.rotate(45, 0, 0, 1);
+    backFin1.matrix.scale(0.9, 0.7, 0.2);
+    backFin1.matrix.translate(-0.5, 0.0, 0.0);
+    backFin1.render();
+    
+    var backFin1Top = new Cube();
+    backFin1Top.color = bodyColor;
+    backFin1Top.matrix = new Matrix4(backFin1.matrix);
+    backFin1Top.matrix.translate(1.0, 0.25, 0.25);
+    backFin1Top.matrix.scale(0.5, 0.5, 0.5);
+    backFin1Top.render();
+    
+    var backFin2 = new Cube();
+    backFin2.color = bodyColor;
+    backFin2.matrix = new Matrix4(body5.matrix);
+    backFin2.matrix.translate(1.0, 0.0, 0.35);
+    backFin2.matrix.rotate(-35, 0, 0, 1);
+    backFin2.matrix.scale(0.8, 0.5, 0.2);
+    backFin2.matrix.translate(-0.5, 0.0, 0.0);
+    backFin2.render();
+    
+    var backFin2Top = new Cube();
+    backFin2Top.color = bodyColor;
+    backFin2Top.matrix = new Matrix4(backFin2.matrix);
+    backFin2Top.matrix.translate(0.85, 0.25, 0.25);
+    backFin2Top.matrix.scale(0.5, 0.5, 0.5);
+    backFin2Top.render();
+    
+    
+    
 }
-         
-function drawPicture() {
-        // Set background to blue
-        gl.clearColor(0.5, 0.7, 1.0, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        
-        // Draw the sun
-        gl.uniform4f(u_FragColor, 1.0, 0.85, 0.45, 1.0);
-        drawTriangle([-1.0, 1.0, -0.6196, 0.8764, -0.6, 1.0]);
-        drawTriangle([-1.0, 1.0, -0.6764, 0.7649, -0.6196, 0.8764]);
-        drawTriangle([-1.0, 1.0, -0.7649, 0.6764, -0.6764, 0.7649]);
-        drawTriangle([-1.0, 1.0, -0.8764, 0.6196, -0.7649, 0.6764]);
-        drawTriangle([-1.0, 1.0, -1.0, 0.6, -0.8764, 0.6196]);
-        
-        // Draw the ground
-        gl.uniform4f(u_FragColor, 0.45, 0.70, 0.25, 1.0);
-        drawTriangle([-1.0, -0.4, -1.0, -1.0, 1.0, -1.0]);
-        drawTriangle([-1.0, -0.4, 1.0, -1.0, 1.0, -0.4]);
-        
-        // Draw the lake
-        gl.uniform4f(u_FragColor, 0.25, 0.35, 1.0, 1.0);
-        drawTriangle([-0.3, -0.45, -0.3, -0.95, 0.3, -0.45]);
-        drawTriangle([-0.3, -0.95, 0.3, -0.95, 0.3, -0.45]);
-        drawTriangle([-0.3, -0.45, -0.5, -0.6, -0.3, -0.7]);
-        drawTriangle([-0.3, -0.7, -0.5, -0.6, -0.5, -0.8]);
-        drawTriangle([-0.5, -0.8, -0.3, -0.95, -0.3, -0.7]);
-        drawTriangle([0.3, -0.45, 0.5, -0.6, 0.3, -0.7]);
-        drawTriangle([0.3, -0.7, 0.5, -0.6, 0.5, -0.8]);
-        drawTriangle([0.5, -0.8, 0.3, -0.95, 0.3, -0.7]);
-        
-        // Draw the mountains
-        gl.uniform4f(u_FragColor, 0.6, 0.6, 0.6, 1.0);
-        drawTriangle([-1.1, -0.4, -0.3, -0.4, -0.7, 0.2]); // Left
-        drawTriangle([1.1, -0.4, 0.3, -0.4, 0.7, 0.2]); // Right
-        gl.uniform4f(u_FragColor, 0.5, 0.5, 0.5, 1.0);
-        drawTriangle([-0.5, -0.4, 0.5, -0.4, 0.0, 0.5]); // Center
-        
-        // Draw the snow
-        gl.uniform4f(u_FragColor, 1.0, 1.0, 1.0, 1.0);
-        drawTriangle([0.0, 0.5, -0.225, 0.095, 0.225, 0.095]); // Center
-        drawTriangle([-0.225, 0.095, -0.15, 0.02, -0.075, 0.095]);
-        drawTriangle([-0.075, 0.095, 0.0, 0.02, 0.075, 0.095]);
-        drawTriangle([0.225, 0.095, 0.15, 0.02, 0.075, 0.095]);
-        drawTriangle([-0.7, 0.2, -0.85, -0.025, -0.55, -0.025]); // Left
-        drawTriangle([-0.85, -0.025, -0.8, -0.075, -0.75, -0.025]);
-        drawTriangle([-0.75, -0.025, -0.7, -0.075, -0.65, -0.025]);
-        drawTriangle([-0.65, -0.025, -0.6, -0.075, -0.55, -0.025]);
-        drawTriangle([0.7, 0.2, 0.85, -0.025, 0.55, -0.025]); // Right
-        drawTriangle([0.85, -0.025, 0.8, -0.075, 0.75, -0.025]);
-        drawTriangle([0.75, -0.025, 0.7, -0.075, 0.65, -0.025]);
-        drawTriangle([0.65, -0.025, 0.6, -0.075, 0.55, -0.025]);
-        
-        // Draw the flower stems
-        gl.uniform4f(u_FragColor, 0.15, 0.5, 0.15, 1.0);
-        drawTriangle([-0.76, -0.9, -0.74, -0.9, -0.75, -0.4]);
-        drawTriangle([-0.75, -0.65, -0.75, -0.6, -0.8, -0.55]);
-        drawTriangle([-0.75, -0.75, -0.75, -0.7, -0.7, -0.65]);
-        
-        drawTriangle([0.76, -0.85, 0.74, -0.85, 0.75, -0.5]);
-        drawTriangle([0.75, -0.75, 0.75, -0.7, 0.7, -0.65]);
-        
-        // Draw the flower center
-        gl.uniform4f(u_FragColor, 0.4, 0.3, 0.1, 1.0);
-        drawTriangle([-0.8, -0.35, -0.8, -0.45, -0.7, -0.35]);
-        drawTriangle([-0.8, -0.45, -0.7, -0.45, -0.7, -0.35]);
-        
-        // Draw the petals
-        gl.uniform4f(u_FragColor, 0.8, 0.6, 0.1, 1.0);
-        drawTriangle([-0.75, -0.35, -0.9, -0.25, -0.8, -0.4]);
-        drawTriangle([-0.8, -0.4, -0.9, -0.55, -0.75, -0.45]);
-        drawTriangle([-0.75, -0.45, -0.6, -0.55, -0.7, -0.4]);
-        drawTriangle([-0.7, -0.4, -0.6, -0.25, -0.75, -0.35]);
-        drawTriangle([0.725, -0.55, 0.775, -0.55, 0.75, -0.45]);
-        drawTriangle([0.725, -0.55, 0.775, -0.55, 0.75, -0.6]);
-}
-
-
