9c0484b40a4fea7227f72a3b943dd4585e868f22
Alexandra Hatalsky <alexhatalsky@MacBook-Pro.local>
2024-05-19 17:20:40 -0700

    Assignment 5B working

--- Asgn5/src/asgn5.js
+++ Asgn5/src/asgn5.js
@@ -3,113 +3,380 @@ import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
 import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
 
-function main() {
-    
-    const canvas = document.querySelector( '#c' );
-    const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
-    
+// Global vars
+let canvas;
+let cameraPos = [0, 0, 500];
+let cameraTarget = [0, 0, 0];
+let controls;
+let camera;
+
+function cameraSetup() {
     const fov = 45;
     const aspect = 2; // the canvas default
-    const near = 0.1;
-    const far = 100;
-    const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-    camera.position.set(0, 20, 50);
+    const near = 1.0;
+    const far = 2000;
+    camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.set(cameraPos[0], cameraPos[1], cameraPos[2]);
     
-    const controls = new OrbitControls( camera, canvas );
-    controls.target.set( 0, 0, 0 );
+    controls = new OrbitControls( camera, canvas );
+    controls.target.set( cameraTarget[0], cameraTarget[1], cameraTarget[2] );
     controls.update();
+}
+
+function main() {
+    canvas = document.querySelector( '#c' );
+    const renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        canvas,
+        alpha: true,
+    });
+
+    cameraSetup();
     
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color( 'black' );
-    
+    let bgTexture;
     {
-        // Plane to represent the ocean
-        const planeSize = 60;
-        
+        // load in the background texture
         const loader = new THREE.TextureLoader();
-        const texture = loader.load( '../lib/images/water.png' );
-        texture.wrapS = THREE.RepeatWrapping;
-        texture.wrapT = THREE.RepeatWrapping;
-        texture.magFilter = THREE.NearestFilter;
-        texture.colorSpace = THREE.SRGBColorSpace;
-        const repeats = planeSize / 2;
-        texture.repeat.set( repeats, repeats );
-        
-        const planeGeo = new THREE.PlaneGeometry( planeSize, planeSize );
-        const planeMat = new THREE.MeshPhongMaterial( {
-        map: texture,
-        side: THREE.DoubleSide,
+        bgTexture = loader.load('../lib/images/stars.jpg');
+        bgTexture.colorSpace = THREE.SRGBColorSpace;
+        scene.background = bgTexture;
+    }
+
+    {
+        // Sphere to be the base world
+        const radius = 100;
+        const widthDivisions = 64;
+        const heightDivisions = 32;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x45D4F7} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(0, 0, 0 );
+        scene.add(mesh);
+    }
+
+    // Shapes to add land to the world;
+    {
+        const radius = 80;
+        const widthDivisions = 64;
+        const heightDivisions = 32;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x73C42B} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(0, 21.5, 0 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 60;
+        const widthDivisions = 64;
+        const heightDivisions = 32;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x73C42B} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(-20, 37, 10 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 60;
+        const widthDivisions = 64;
+        const heightDivisions = 32;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x228A0E} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(-5, 37, 20 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 70;
+        const widthDivisions = 64;
+        const heightDivisions = 32;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x73C42B} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(-35, 5, 0 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 70;
+        const widthDivisions = 64;
+        const heightDivisions = 32;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x73C42B} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(-30, -20, 0 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 80;
+        const widthDivisions = 64;
+        const heightDivisions = 32;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x73C42B} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(5, -15, 18 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 70;
+        const widthDivisions = 64;
+        const heightDivisions = 32;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x228A0E} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(7, -23.5, 25.5 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 60;
+        const widthDivisions = 64;
+        const heightDivisions = 32;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x73C42B} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(15, -10, 40 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 60;
+        const widthDivisions = 64;
+        const heightDivisions = 32;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x73C42B} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(15, 5, -38 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 60;
+        const widthDivisions = 64;
+        const heightDivisions = 32;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x228A0E} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(5, 15, -37 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 10;
+        const height = 20;
+        const radialsegments = 20;
+        const geometry = new THREE.ConeGeometry( radius, height, radialsegments );
+        geometry.rotateX(Math.PI/16);
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x753913} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(-25, 99, 30 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 5;
+        const height = 10;
+        const radialsegments = 20;
+        const geometry = new THREE.ConeGeometry( radius, height, radialsegments );
+        geometry.rotateX(Math.PI/16);
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x753913} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(-15, 97, 35 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 20;
+        const height = 30;
+        const radialsegments = 20;
+        const geometry = new THREE.ConeGeometry( radius, height, radialsegments );
+        geometry.rotateZ(Math.PI/2);
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x753913} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(-105, 0, 20 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 25;
+        const height = 35;
+        const radialsegments = 20;
+        const geometry = new THREE.ConeGeometry( radius, height, radialsegments );
+        geometry.rotateZ(5 * Math.PI/9);
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x753913} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(-105, -15, 5 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 10;
+        const height = 20;
+        const radialsegments = 20;
+        const geometry = new THREE.ConeGeometry( radius, height, radialsegments );
+        geometry.rotateZ(4 * Math.PI/7);
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x753913} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(-105, -30, -10 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 15;
+        const height = 25;
+        const radialsegments = 20;
+        const geometry = new THREE.ConeGeometry( radius, height, radialsegments );
+        geometry.rotateZ(3 * Math.PI/5);
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x753913} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(-95, -45, 15 );
+        scene.add(mesh);
+    }
+    {
+        const radius = 10;
+        const height = 20;
+        const radialsegments = 20;
+        const geometry = new THREE.ConeGeometry( radius, height, radialsegments );
+        geometry.rotateZ(-3 * Math.PI/7);
+        geometry.rotateY(Math.PI/3);
+
+        const mat = new THREE.MeshPhongMaterial( {color: 0x753913} );
+        const mesh = new THREE.Mesh( geometry, mat );
+        mesh.position.set(40, 10, -95 );
+        scene.add(mesh);
+    }
+
+    let jupiter;
+    {
+        // Jupiter
+        const radius = 50;
+        const widthDivisions = 64;
+        const heightDivisions = 32;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
+
+        const loader = new THREE.TextureLoader();
+        const texture = loader.load( '../lib/images/jupiter.jpg' );
+        const mat = new THREE.MeshPhongMaterial( {
+            map: texture,
         } );
-        const mesh = new THREE.Mesh( planeGeo, planeMat );
-        mesh.rotation.x = Math.PI * - .5;
-        scene.add( mesh );
+        jupiter = new THREE.Mesh( geometry, mat );
+        jupiter.position.set(300, 10, 0 );
+        scene.add(jupiter);
+    }
+
+    let sun;
+    {
+        // Sphere to represent the sun
+        const radius = 30;
+        const widthDivisions = 32;
+        const heightDivisions = 16;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
         
+        const mat = new THREE.MeshBasicMaterial( {color: 0xFCCC62} );
+        sun = new THREE.Mesh( geometry, mat );
+        sun.position.set(15, 0, 0 );
+        scene.add( sun );
+    }
+
+    let moon;
+    {
+        // Sphere to represent the moon
+        const radius = 15;
+        const widthDivisions = 32;
+        const heightDivisions = 16;
+        const geometry = new THREE.SphereGeometry( radius, widthDivisions, heightDivisions );
+
+        const loader = new THREE.TextureLoader();
+        const texture= loader.load( '../lib/images/moon.jpg' );
+        texture.colorSpace = THREE.SRGBColorSpace;
+        
+        const mat = new THREE.MeshPhongMaterial({
+            map: texture,
+        });
+        
+        moon = new THREE.Mesh( geometry, mat );
+        moon.position.set(15, 0, 0 );
+        scene.add( moon );
     }
     
-    // Sphere to represent the sun
-    const sphereRadius = 3;
-    const sphereWidthDivisions = 32;
-    const sphereHeightDivisions = 16;
-    const sphereGeo = new THREE.SphereGeometry( sphereRadius, sphereWidthDivisions, sphereHeightDivisions );
-    
-    const sphereMat = new THREE.MeshBasicMaterial( {color: 0xFCCC62} );
-    const meshSphere = new THREE.Mesh( sphereGeo, sphereMat );
-    meshSphere.position.set(15, 0, 0 );
-    scene.add( meshSphere );
-
-    // Sphere to represent the moon
-    const loader = new THREE.TextureLoader();
-    const texture= loader.load( '../lib/images/moon.jpg' );
-    texture.colorSpace = THREE.SRGBColorSpace;
-    
-    const moonMat = new THREE.MeshPhongMaterial({
-        map: texture,
-    });
-    
-    const meshSphere2 = new THREE.Mesh( sphereGeo, moonMat );
-    meshSphere2.position.set(15, 0, 0 );
-    scene.add( meshSphere2 );
-    
-    // Octahedron to represent a star
-    const octoRadius = 1;
+    // Octahedrons to represent stars
+    const octoRadius = 3;
     const octoGeometry = new THREE.OctahedronGeometry( octoRadius );
     const octoMat = new THREE.MeshBasicMaterial( {color: 0xFFF8D4} );
-    const octoMesh = new THREE.Mesh(octoGeometry, octoMat);
-    octoMesh.position.set(20, 15, 0);
-    scene.add(octoMesh)
+    const octoMesh1 = new THREE.Mesh(octoGeometry, octoMat);
+    const octoMesh2 = new THREE.Mesh(octoGeometry, octoMat);
+    const octoMesh3 = new THREE.Mesh(octoGeometry, octoMat);
+    const octoMesh4 = new THREE.Mesh(octoGeometry, octoMat);
+    const octoMesh5 = new THREE.Mesh(octoGeometry, octoMat);
+    const octoMesh6 = new THREE.Mesh(octoGeometry, octoMat);
+    octoMesh1.position.set(20, 15, 0);
+    octoMesh2.position.set(20, 15, 0);
+    octoMesh3.position.set(20, 15, 0);
+    octoMesh4.position.set(20, 15, 0);
+    octoMesh5.position.set(20, 15, 0);
+    octoMesh6.position.set(20, 15, 0);
+    scene.add(octoMesh1);
+    scene.add(octoMesh2);
+    scene.add(octoMesh3);
+    scene.add(octoMesh4);
+    scene.add(octoMesh5);
+    scene.add(octoMesh6);
     
-    
-    // Load in the mountain object
+    let rock;
     {
+        // Load in the meteor object
         const objLoader = new OBJLoader();
         const mtlLoader = new MTLLoader();
-        mtlLoader.load('../lib/models/mountain/part.mtl', (mtl) => {
+        mtlLoader.load('../lib/models/meteor/materials.mtl', (mtl) => {
             mtl.preload();
             objLoader.setMaterials(mtl);
-        objLoader.load('../lib/models/mountain/part.obj', (root) => {
+        objLoader.load('../lib/models/meteor/model.obj', (root) => {
+            root.scale.setScalar(20);
+            root.position.x = 0;
+            root.position.y = 150;
+            root.position.z = 0;
+            rock = root;
             scene.add(root);
         });
         });
     }
     
-    // Directional light to rotate around the canvas with the sun, pointing at the center (0, 0, 0)
-    const color = 0xFFFFFF;
-    const intensity1 = 4;
-    const light1 = new THREE.DirectionalLight(color, intensity1);
-    light1.position.set(10, 10, 0);
-    light1.target.position.set(0, 0, 0);
-    scene.add(light1);
-    scene.add(light1.target);
-    
+    let sunlight;
     {
-        
+        // Directional light to rotate around the canvas with the sun, pointing at the center (0, 0, 0)
+        const color = 0xFFFFFF;
+        const intensity = 4;
+        sunlight = new THREE.DirectionalLight(color, intensity);
+        sunlight.castShadow = true;
+        sunlight.position.set(10, 10, 0);
+        sunlight.target.position.set(0, 0, 0);
+        scene.add(sunlight);
+        scene.add(sunlight.target);
+    }
+
+    {
+        // Point light for the origin
+        const color = 0x8888FF;
+		const intensity = 100000;
+		const light = new THREE.PointLight( color, intensity );
+		light.position.set( 0, 0, 0 );
+		scene.add( light );
+    }
+
+    {
         // Hemisphere light
-        const skyColor = 0x0000FF;
-        const groundColor = 0x000000;
-        const intensity3 = 1;
-        const light3 = new THREE.HemisphereLight(skyColor, groundColor, intensity3);
-        scene.add(light3);
-        
+        const skyColor = 0x330066;
+        const groundColor = 0x330066;
+        const intensity = 3;
+        const hemisphereLight = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+        scene.add(hemisphereLight);
     }
 
     function resizeRendererToDisplaySize( renderer ) {
@@ -132,7 +399,6 @@ function main() {
         time *= 0.0005;
 
         if ( resizeRendererToDisplaySize( renderer ) ) {
-
             const canvas = renderer.domElement;
             camera.aspect = canvas.clientWidth / canvas.clientHeight;
             camera.updateProjectionMatrix();
@@ -140,14 +406,49 @@ function main() {
         }
         
         // Make the shapes orbit the origin
-        meshSphere.position.x = 25 * Math.cos(time);
-        meshSphere.position.y = 25 * Math.sin(time);
-        meshSphere2.position.x = 20 * Math.cos((time + Math.PI)* 0.8);
-        meshSphere2.position.y = 20 * Math.sin((time + Math.PI) * 0.8);
-        octoMesh.position.x = 30 * Math.sin(time);
-        octoMesh.position.z = 30 * Math.cos(time);
-        light1.position.x = 25 * Math.cos(time);
-        light1.position.y = 25 * Math.sin(time)
+        sun.position.x = 500 * Math.cos(time);
+        sun.position.y = 500 * Math.sin(time);
+        moon.position.x = 160 * Math.cos((time + Math.PI)* 0.8);
+        moon.position.y = 160 * Math.sin((time + Math.PI) * 0.8);
+
+        let starSpeed = 5;
+        let starLoc = 300
+
+        octoMesh1.position.x = starLoc * Math.sin(time/starSpeed);
+        octoMesh1.position.z = starLoc * Math.cos(time/starSpeed);
+
+        octoMesh2.position.x = starLoc * Math.sin(time/starSpeed + 5);
+        octoMesh2.position.y = starLoc * Math.cos(time/starSpeed + 5);
+
+        octoMesh3.position.x = starLoc * Math.sin(time/starSpeed + 20);
+        octoMesh3.position.y = starLoc * Math.cos(time/starSpeed + 20);
+        octoMesh3.position.z = starLoc * Math.cos(time/starSpeed + 20);
+
+        octoMesh4.position.y = starLoc * Math.sin(time/starSpeed);
+        octoMesh4.position.z = starLoc * Math.cos(time/starSpeed);
+
+        octoMesh5.position.x = starLoc * Math.sin(time/starSpeed);
+        octoMesh5.position.y = starLoc * Math.cos(time/starSpeed + 10);
+        octoMesh5.position.z = starLoc * Math.cos(time/starSpeed);
+
+        octoMesh6.position.x = starLoc * Math.sin(time/starSpeed + 10);
+        octoMesh6.position.y = starLoc/2 * Math.sin(time/starSpeed + 20);
+        octoMesh6.position.z = starLoc * Math.cos(time/starSpeed + 30);
+
+        sunlight.position.x = 500 * Math.cos(time);
+        sunlight.position.y = 500 * Math.sin(time)
+
+        jupiter.position.x = 300 * Math.cos(time);
+        jupiter.position.z = 300 * Math.sin(time);
+        jupiter.rotation.y += 0.002;
+
+        if (rock) {
+            rock.position.x = 200 * Math.cos(2 * time);
+            rock.position.y = 200 * Math.sin(2 * time);
+            rock.position.z = 200 * Math.cos(2 * time);
+            rock.rotation.x += 0.01;
+            rock.rotation.y += 0.01;
+        }
 
         renderer.render( scene, camera );
 
